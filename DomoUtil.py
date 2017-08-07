cf5aa11b3e6c9d343e67b1f37bf42329d296493e
Unknown <irwin.phillip@gmail.com>
2017-08-07 16:12:34 -0500

    Finished moving defs.

--- DomoUtil.py
+++ DomoUtil.py
@@ -7,6 +7,7 @@ Confidential
 
 import DomoUtilHelper as dmo
 import argparse
+from pydomo import Schema
 
 count = 0
 dtype_df = None
@@ -23,148 +24,6 @@ def searchStream(name):
     stream = streams.search(stream_prop)
     return stream
 
-def deleteStream(stream):    
-    domo.streams.delete(stream.id)
-    domo.datasets.delete(stream.dataSet.id)
-
-
-
-def uploadPart(arglist):
-    dmo = arglist[0]
-    
-    for i in range(5):
-            try:
-                
-                execution = dmo.streams.upload_part(arglist[1], arglist[2],arglist[3], csv = open(arglist[4], 'rb'))
-                
-                if execution is None:
-                    raise Exception('Error uploading part ' + str(arglist[3]) + ' Retrying...')
-                    
-            except Exception as e:
-                print(e)
-                #logging.warning(str(arglist[2]) + ' Failed on part ' + str(arglist[3]) + '... retrying in 5 sec')
-                time.sleep(5)
-                continue
-            break
-    
-    
-def uploadStream(execution, stream, filelist):
-    print('Starting Upload')
-    t = time.time()
-    i = 0
-    args = list()
-    for file in filelist:
-        i+=1
-        args.append([domo, stream.id, execution.id, i, file])
-
-    
-    async def upload():
-    
-        with concurrent.futures.ThreadPoolExecutor(max_workers=24) as executor:
-      
-            loop = asyncio.get_event_loop()
-            futures = [
-                loop.run_in_executor(
-                    executor, 
-                    uploadPart,
-                    arg
-                )
-                for arg in args
-            ]
-            for response in await asyncio.gather(*futures):
-                pass
-    
-    
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(upload())
-    
-        
-    domo.streams.commit_execution(stream.id, execution.id)
-    print('Completed Stream Upload in ' + str(time.time()-t) + ' secs...')
-
-
-
-def readData(sql, temp_dir, rowsper=100000):
-    print('Reading data...')
-    cnxn = pyodbc.connect('DRIVER={NetezzaSQL};SERVER=SRVDWHITP01;DATABASE=EDW_SPOKE;UID=pairwin;PWD=pairwin;TIMEOUT=0')   
-    i = 0
-    for chunk in pd.read_sql(sql, cnxn, chunksize=rowsper) :
-        if i == 0:
-            dtype_df = chunk.dtypes.reset_index()
-            dtype_df.columns = ["Count", "Column Type"]
-        chunk.to_csv(temp_dir+'\\file'+str(i)+'.gzip',index=False, compression='gzip', header=False)
-        #print(temp_dir+'\\file'+str(i))
-        i+=1
-    print('Data read complete...')
-    return dtype_df
-
-
-def writePart(data, tdir):
-    #print(data)
-    global count 
-    global dtype_df
-    if count == 0:
-            dtype_df = data.dtypes.reset_index()
-            dtype_df.columns = ["Count", "Column Type"]
-    #print(df)
-    file = tdir + '\\file' + str(count)
-    print(file)
-    data.to_csv(file, index=False, compression='gzip', header=False)
-    count += 1
-
-
-
-# try fetch many...
-#https://stackoverflow.com/questions/7555680/create-db-connection-and-maintain-on-multiple-processes-multiprocessing
-def readDataAsync(sql, temp_dir, rowsper=100000):    
-    print('Reading data...')
-    cnxn = pyodbc.connect('DRIVER={NetezzaSQL};SERVER=SRVDWHITP01;DATABASE=EDW_SPOKE;UID=pairwin;PWD=pairwin;TIMEOUT=0') 
-        
-    loop = asyncio.get_event_loop()
-
-
-    async def readChunk(tdir, rowsper):
-    
-        with concurrent.futures.ThreadPoolExecutor(max_workers=24) as executor:
-      
-            loop = asyncio.get_event_loop()
-            futures = [
-                loop.run_in_executor(
-                    executor, 
-                    writePart,
-                    chunk,
-                    temp_dir
-                )
-                for chunk in pd.read_sql(sql, cnxn, chunksize=rowsper)
-            ]
-            for response in await asyncio.gather(*futures):
-                pass
-    
-    loop.run_until_complete(readChunk(temp_dir, rowsper))
-
-    
-
-
-        
-        
-        
-
-
-def buildSchema(df):
-    print('Building Schema...')
-    sclist = list()
-    for row in df.itertuples():
-        if str(row[2]) == 'int64':
-            sclist.append(Column(ColumnType.LONG, row[1]))
-        elif str(row[2]) == 'float64': 
-            sclist.append(Column(ColumnType.DECIMAL, row[1]))
-        elif str(row[2]) == r'datetime64[ns]': 
-            sclist.append(Column(ColumnType.DATETIME, row[1]))
-        elif str(row[2]) == r'date64[ns]': 
-            sclist.append(Column(ColumnType.DATE, row[1]))
-        elif str(row[2]) == 'object': 
-            sclist.append(Column(ColumnType.STRING, row[1]))
-    return sclist
 
 
 
@@ -187,7 +46,7 @@ def main(args):
         if args.sqlfile is not None:
             sql = domo.getSQL(args.sqlfile)    
 #            schemadf = readData(sql, temp_dir, rowsper)
-            readDataAsync(sql, temp_dir, rowsper)
+            domo.readDataAsync(sql, temp_dir, rowsper)
             fl = domo.buildfilelist(temp_dir)
         
         if args.name is not None:
@@ -203,7 +62,7 @@ def main(args):
             strm = domo.streams.get(strm_id) #stream id
             print('Updating ' + strm.dataSet.name)
             exe = domo.createExecution(strm)
-            uploadStream(exe, strm, fl)
+            domo.uploadStream(exe, strm, fl)
             
         if args.delete is not None:
             dataSourceId = args.delete
@@ -215,14 +74,14 @@ def main(args):
 
             strm = domo.streams.get(strm_id) #stream id
             print('Deleting ' + strm.dataSet.name)
-            deleteStream(strm)
+            domo.deleteStream(strm)
             
         
         if args.create:
-            SCHEMA = Schema(buildSchema(dtype_df))  
-            strm = createStream(name, SCHEMA)
-            exe = createExecution(strm)
-            uploadStream(exe, strm, fl)
+            SCHEMA = Schema(domo.buildSchema(dtype_df))  
+            strm = domo.createStream(name, SCHEMA)
+            exe = domo.createExecution(strm)
+            domo.uploadStream(exe, strm, fl)
     
     except Exception as err:
         print(err)
--- DomoUtilHelper.py
+++ DomoUtilHelper.py
@@ -17,6 +17,11 @@ import tempfile
 import shutil
 import os
 import logging
+import pandas as pd
+import pyodbc
+import time
+import concurrent.futures
+import asyncio
 
 class DomoSDK:
     def __init__(self):
@@ -36,6 +41,7 @@ class DomoSDK:
 
         self.domo = Domo(client_id, client_secret, api_host, logger_name='foo', logger_level=logging.INFO, use_https=True)
         self.logger = self.domo.logger
+        self.streams = self.domo.streams
         
         
     def getSQL(path):
@@ -69,26 +75,165 @@ class DomoSDK:
     
         
     def listStreams(self):
-        streams = self.domo.streams
         limit = 1000
         offset = 0
-        stream_list = streams.list(limit, offset)
+        stream_list = self.streams.list(limit, offset)
         return stream_list
         
     def createStream(self, name, schem):
-        streams = self.domo.streams
         print('Creating Stream ' + name + '...')
         dsr = DataSetRequest()
         dsr.name = name
         dsr.schema = schem
         stream_request = CreateStreamRequest(dsr, UpdateMethod.REPLACE)
-        stream = streams.create(stream_request)
+        stream = self.streams.create(stream_request)
         print('Stream ID: ' + str(stream.id))
         return stream
     
     
     def createExecution(self, strm):
         print('Creating execution...')
-        streams = self.domo.streams
-        execution = streams.create_execution(strm.id)
+        execution = self.streams.create_execution(strm.id)
         return execution
+    
+    def deleteStream(self, stream):    
+        self.streams.delete(stream.id)
+        self.datasets.delete(stream.dataSet.id)
+        
+        
+    def readData(sql, temp_dir, rowsper=100000):
+        print('Reading data...')
+        cnxn = pyodbc.connect('DRIVER={NetezzaSQL};SERVER=SRVDWHITP01;DATABASE=EDW_SPOKE;UID=pairwin;PWD=pairwin;TIMEOUT=0')   
+        i = 0
+        for chunk in pd.read_sql(sql, cnxn, chunksize=rowsper) :
+            if i == 0:
+                dtype_df = chunk.dtypes.reset_index()
+                dtype_df.columns = ["Count", "Column Type"]
+            chunk.to_csv(temp_dir+'\\file'+str(i)+'.gzip',index=False, compression='gzip', header=False)
+            #print(temp_dir+'\\file'+str(i))
+            i+=1
+        print('Data read complete...')
+        return dtype_df
+    
+
+        
+        
+    def uploadStream(self, execution, stream, filelist):
+        print('Starting Upload')
+        t = time.time()
+        i = 0
+        args = list()
+        for file in filelist:
+            i+=1
+            args.append([stream.id, execution.id, i, file])
+    
+        def uploadPart(self, arglist):
+            
+            for i in range(5):
+                    try:
+                        
+                        execution = self.streams.upload_part(arglist[0], arglist[1],arglist[2], csv = open(arglist[3], 'rb'))
+                        
+                        if execution is None:
+                            raise Exception('Error uploading part ' + str(arglist[3]) + ' Retrying...')
+                            
+                    except Exception as e:
+                        print(e)
+                        #logging.warning(str(arglist[2]) + ' Failed on part ' + str(arglist[3]) + '... retrying in 5 sec')
+                        time.sleep(5)
+                        continue
+                    break
+        async def upload(self):
+        
+            with concurrent.futures.ThreadPoolExecutor(max_workers=24) as executor:
+          
+                loop = asyncio.get_event_loop()
+                futures = [
+                    loop.run_in_executor(
+                        executor, 
+                        uploadPart,
+                        self,
+                        arg
+                    )
+                    for arg in args
+                ]
+                for response in await asyncio.gather(*futures):
+                    pass
+        
+        
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(upload())
+        
+            
+        self.streams.commit_execution(stream.id, execution.id)
+        print('Completed Stream Upload in ' + str(time.time()-t) + ' secs...')
+    
+    
+    
+    
+    
+    
+
+    
+    
+    
+    # try fetch many...
+    #https://stackoverflow.com/questions/7555680/create-db-connection-and-maintain-on-multiple-processes-multiprocessing
+    def readDataAsync(self, sql, temp_dir, rowsper=100000):    
+        print('Reading data...')
+        cnxn = pyodbc.connect('DRIVER={NetezzaSQL};SERVER=SRVDWHITP01;DATABASE=EDW_SPOKE;UID=pairwin;PWD=pairwin;TIMEOUT=0') 
+            
+        loop = asyncio.get_event_loop()
+        
+        
+        def writePart(self, data, tdir):
+            #print(data)
+            global count 
+            global dtype_df
+            if count == 0:
+                    dtype_df = data.dtypes.reset_index()
+                    dtype_df.columns = ["Count", "Column Type"]
+            #print(df)
+            file = tdir + '\\file' + str(count)
+            print(file)
+            data.to_csv(file, index=False, compression='gzip', header=False)
+            count += 1
+        
+        async def readChunk(tdir, rowsper):
+        
+            with concurrent.futures.ThreadPoolExecutor(max_workers=24) as executor:
+          
+                loop = asyncio.get_event_loop()
+                futures = [
+                    loop.run_in_executor(
+                        executor, 
+                        writePart,
+                        self,
+                        chunk,
+                        temp_dir
+                    )
+                    for chunk in pd.read_sql(sql, cnxn, chunksize=rowsper)
+                ]
+                for response in await asyncio.gather(*futures):
+                    pass
+        
+        loop.run_until_complete(readChunk(temp_dir, rowsper))
+        
+
+
+    def buildSchema(df):
+        print('Building Schema...')
+        sclist = list()
+        for row in df.itertuples():
+            if str(row[2]) == 'int64':
+                sclist.append(Column(ColumnType.LONG, row[1]))
+            elif str(row[2]) == 'float64': 
+                sclist.append(Column(ColumnType.DECIMAL, row[1]))
+            elif str(row[2]) == r'datetime64[ns]': 
+                sclist.append(Column(ColumnType.DATETIME, row[1]))
+            elif str(row[2]) == r'date64[ns]': 
+                sclist.append(Column(ColumnType.DATE, row[1]))
+            elif str(row[2]) == 'object': 
+                sclist.append(Column(ColumnType.STRING, row[1]))
+        return sclist
+
